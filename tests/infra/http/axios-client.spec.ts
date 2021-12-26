9c9205f91a988f61950198969d65e419b275f237
Tiago Jose <tiago_jvo@outlook.com>
2021-12-26 16:45:59 -0300

    refactor: move production code to their own files

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -1,15 +1,8 @@
-import { HttpGetClient } from '@/infra/http'
 
+import { AxiosHttpClient } from '@/infra/http'
 import axios from 'axios'
 
 jest.mock('axios')
-
-class AxiosHttpClient {
-  async get <T = any> (args: HttpGetClient.Params): Promise<T> {
-    const response = await axios.get(args.url, { params: args.params })
-    return response.data
-  }
-}
 
 describe('AxiosClient', () => {
   let sut: AxiosHttpClient
