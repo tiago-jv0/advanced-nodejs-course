13bee2edfe19710dcc36f1d8df99dc2874e21092
Tiago Jose <tiago_jvo@outlook.com>
2021-12-29 01:44:06 -0300

    feat: ensure RequiredStringValidator returns error if value is empty

---
+++ tests/application/validations/required-string.spec.ts
@@ -0,0 +1,39 @@
+import { RequiredFieldError } from '@/application/errors'
+
+class RequiredStringValidator {
+  constructor (private readonly fieldName: string, private readonly value: string) {}
+
+  validate (): Error | undefined {
+    if (this.value === '' || this.value === undefined || this.value === null) {
+      return new RequiredFieldError(this.fieldName)
+    }
+
+    return undefined
+  }
+}
+
+describe('RequiredStringValidator', () => {
+  it('should return RequiredFieldError if value is empty', () => {
+    const sut = new RequiredStringValidator('any_field', '')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+
+  it('should return RequiredFieldError if value is null', () => {
+    const sut = new RequiredStringValidator('any_field', null as any)
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+
+  it('should return RequiredFieldError if value is undefined', () => {
+    const sut = new RequiredStringValidator('any_field', undefined as any)
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+})
