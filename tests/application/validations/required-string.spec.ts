67a6d7d1083625379503c66a0714152ef38c65d1
Tiago Jose <tiago_jvo@outlook.com>
2021-12-29 01:45:53 -0300

    feat: ensure RequiredStringValidator returns undefined if value is not empty

--- tests/application/validations/required-string.spec.ts
+++ tests/application/validations/required-string.spec.ts
@@ -36,4 +36,12 @@ describe('RequiredStringValidator', () => {
 
     expect(error).toEqual(new RequiredFieldError('any_field'))
   })
+
+  it('should return undefined if value is not empty', () => {
+    const sut = new RequiredStringValidator('any_field', 'any_value')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
 })
