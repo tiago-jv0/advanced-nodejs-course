919f94f982dea940f03909efa9a38d7896dfb54f
Tiago Jose <tiago_jvo@outlook.com>
2021-12-28 21:31:30 -0300

    refactor: apply unauthorized helper

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,7 +1,7 @@
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
 import { RequiredFieldError, ServerError } from '@/application/errors'
-import { badRequest, HttpResponse } from '@/application/helpers'
+import { badRequest, HttpResponse, unauthorized } from '@/application/helpers'
 
 export class FacebookLoginController {
   constructor (private readonly facebookAuthentication: FacebookAuthentication) {}
@@ -13,20 +13,17 @@ export class FacebookLoginController {
       if (invalidTokenCases.includes(httpRequest.token)) {
         return badRequest(new RequiredFieldError('token'))
       }
-      const result = await this.facebookAuthentication.perform({ token: httpRequest.token })
+      const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
 
-      if (result instanceof AccessToken) {
+      if (accessToken instanceof AccessToken) {
         return {
           statusCode: 200,
           data: {
-            accessToken: result.value
+            accessToken: accessToken.value
           }
         }
       } else {
-        return {
-          statusCode: 401,
-          data: result
-        }
+        return unauthorized()
       }
     } catch (error) {
       return { statusCode: 500, data: new ServerError(error as Error) }
--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -1,6 +1,12 @@
+import { UnauthorizedError } from '@/application/errors'
 export type HttpResponse = {statusCode: number, data: any}
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   data: error
+})
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  data: new UnauthorizedError()
 })
