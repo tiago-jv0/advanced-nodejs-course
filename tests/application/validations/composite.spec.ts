eee98dad5504703ffade294f0306e092a2adafdd
Tiago Jose <tiago_jvo@outlook.com>
2021-12-29 05:28:47 -0300

    feat: ensure ValidationComposite returns the first error found

--- tests/application/validations/composite.spec.ts
+++ tests/application/validations/composite.spec.ts
@@ -1,9 +1,16 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
-class ValidationComposite {
+class ValidationComposite implements Validator {
   constructor (private readonly validators: Validator[]) {}
 
-  validate (): undefined {
+  validate (): Error | undefined {
+    for (const validator of this.validators) {
+      const error = validator.validate()
+
+      if (error !== undefined) {
+        return error
+      }
+    }
     return undefined
   }
 }
@@ -35,5 +42,14 @@ describe('ValidationComposite', () => {
     const error = sut.validate()
 
     expect(error).toBeUndefined()
+  })
+
+  it('should return the first error', () => {
+    validator1.validate.mockReturnValue(new Error('error_1'))
+    validator2.validate.mockReturnValue(new Error('error_2'))
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new Error('error_1'))
   })
 })
